3d83d75fe383af6bbe213a15b346d478698dabc0
nesrine-ben-romdhane <nesrinebenromdhane441@gmail.com>
2023-05-16 16:39:24 +0200

    first commit

---
+++ src/middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+const auth = async(req,res,next) =>{
+    try{
+        const token = req.header('Authorization').replace('Bearer ','')
+        console.log(token)
+        const decode = jwt.verify(token,'nesrine2')
+        console.log(decode)
+
+        const user = await User.findOne({_id:decode._id,tokens:token})
+        console.log(user)
+        if(!user){
+            throw new Error()
+        }
+        req.user = user
+        req.token = token
+        next()
+    }
+    catch(e){
+        res.status(401).send({error:'Please authenticate'})
+    }
+}
+module.exports = auth
